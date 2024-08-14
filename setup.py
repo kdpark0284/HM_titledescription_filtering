4ed07e112fec89f49378425ce9d4eee613bda234
kdpark0284 <85272306+kdpark0284@users.noreply.github.com>
2024-08-14 23:24:34 +0900

    changed name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="HMautogroup_pre", # 모듈 이름
+    name="HMautogroup", # 모듈 이름
     version="0.1.0", # 버전
     author="KDPark", # 제작자
     author_email="k602511@gmail.com", # contact
@@ -12,6 +12,14 @@ setuptools.setup(
     long_description=open('README.md').read(), # README.md에 보통 모듈 설명을 해놓는다.
     long_description_content_type="text/markdown",
     url="https://github.com/kdpark0284/HM_titledescription_filtering",
+    project_url={
+        "Bug Tracker": "https://github.com/kdpark0284/HM_titledescription_filtering/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[ # 필수 라이브러리들을 포함하는 부분인 것 같음, 다른 방식으로 넣어줄 수 있는지는 알 수 없음
     "openpyxl==3.1.5", 
     "numpy==1.26.4", 
@@ -25,6 +33,7 @@ setuptools.setup(
     "rapidfuzz==3.9.6",
     "jamo==0.4.1"
     ],
+    package_dir={"": "lib"},
     package_data={'': ['LICENSE.txt', 'requirements.txt']}, # 원하는 파일 포함, 제대로 작동되지 않았음
     include_package_data=True,
     packages = setuptools.find_packages(), # 모듈을 자동으로 찾아줌
